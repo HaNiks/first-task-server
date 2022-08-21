ebfa6b9cbcd81924ff2c1f2b7eeb8617c4e92b6e
HaNiks <haniks2899@gmail.com>
2022-08-21 22:22:22 +0300

    change delete mapping

--- src/main/java/com/balinasoft/firsttask/controller/api2/CategoryController.java
+++ src/main/java/com/balinasoft/firsttask/controller/api2/CategoryController.java
@@ -4,7 +4,7 @@ import com.balinasoft.firsttask.dto.CommentDtoOut;
 import com.balinasoft.firsttask.dto.ResponseDto;
 import com.balinasoft.firsttask.dto.api2.CategoryDTOIn;
 import com.balinasoft.firsttask.dto.api2.CategoryDTOOut;
-import com.balinasoft.firsttask.service.api2.CategoryService;
+import com.balinasoft.firsttask.service.api2.CategoryServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -23,30 +23,30 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @RequiredArgsConstructor
 public class CategoryController {
 
-    private final CategoryService categoryService;
+    private final CategoryServiceImpl categoryServiceImpl;
 
     @RequestMapping(value = "", method = POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ApiOperation(value = "Add category", response = CommentDtoOut.class)
     public ResponseDto add(@RequestBody CategoryDTOIn categoryDTOIn) {
-        return wrap(categoryService.save(categoryDTOIn));
+        return wrap(categoryServiceImpl.save(categoryDTOIn));
     }
 
     @RequestMapping(value = "", method = GET)
     @ApiOperation(value = "Find all categories")
     public Page<CategoryDTOOut> findAll(@RequestParam(defaultValue = "0", required = false) int page) {
-        return categoryService.findAll(page);
+        return categoryServiceImpl.findAll(page);
     }
 
     @RequestMapping(value = "name", method = GET)
     @ApiOperation(value = "Find category by name")
     public CategoryDTOOut findByName(@RequestParam String name) {
-        return categoryService.findByName(name);
+        return categoryServiceImpl.findByName(name);
     }
 
-    @RequestMapping(value = "{categoryId}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "{categoryName}", method = RequestMethod.DELETE)
     @ApiOperation(value = "Delete category", response = CategoryDTOOut.class)
-    public ResponseDto delete(@PathVariable int categoryId) {
-        categoryService.delete(categoryId);
+    public ResponseDto delete(@PathVariable String categoryName) {
+        categoryServiceImpl.delete(categoryName);
         return wrap();
     }
 
