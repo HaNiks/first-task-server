3b30f1a11d910bd5deed04b5d956aacc9767a6dc
HaNiks <haniks2899@gmail.com>
2022-08-21 21:07:34 +0300

    add fields to Category

--- src/main/java/com/balinasoft/firsttask/domain/api2/Category.java
+++ src/main/java/com/balinasoft/firsttask/domain/api2/Category.java
@@ -1,4 +1,28 @@
 package com.balinasoft.firsttask.domain.api2;
 
+import com.balinasoft.firsttask.domain.Image;
+import com.balinasoft.firsttask.domain.User;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(length = 1000)
+    private String name;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+
+
+    @OneToMany
+    private List<Image> images;
 }
